0d6e1e0602f63ec1860bc4ccab8efb79503109ac
Khalid Mahmood <khalidM@corp.microsoft.com>
2019-05-25 02:22:30 -0700

    Hangfire StrongName

--- src/Hangfire.Core/IGlobalConfiguration.cs
+++ src/Hangfire.Core/IGlobalConfiguration.cs
@@ -18,14 +18,11 @@ using System.ComponentModel;
 
 namespace Hangfire
 {
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public interface IGlobalConfiguration<out T> : IGlobalConfiguration
     {
-        [EditorBrowsable(EditorBrowsableState.Advanced)]
         T Entry { get; }
     }
 
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public interface IGlobalConfiguration
     {
     }
--- src/Hangfire.SqlServer/Properties/AssemblyInfo.cs
+++ src/Hangfire.SqlServer/Properties/AssemblyInfo.cs
@@ -8,6 +8,16 @@ using System.Runtime.InteropServices;
 [assembly: Guid("3d96bf2f-8854-4872-aee3-faf81d121a4d")]
 [assembly: CLSCompliant(true)]
 
-[assembly: InternalsVisibleTo("Hangfire.SqlServer.Tests")]
+[assembly: InternalsVisibleTo("Hangfire.SqlServer.Tests,PublicKey="+
+"0024000004800000940000000602000000240000525341310004000001000100a32bb060353a2c"+
+"e71364d1c2ad3736f7a19a68cf9868f0a80fe8c924eb3023c69d104d840da9da51fa726c289357"+
+"7af6f51051f8d4c91a45ea11d7c04b632c4b81280796af677e96e6c55830418f13914d02edd54c"+
+"94dbd6f15fab2964dc14003264dcdeaded7fc878dbc9f9908f2ad0a5b8360fccd241f68aee0eb2"+
+"97eeb2a3")]
 // Allow the generation of mocks for internal types
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2,PublicKey="+
+"0024000004800000940000000602000000240000525341310004000001000100a32bb060353a2c"+
+"e71364d1c2ad3736f7a19a68cf9868f0a80fe8c924eb3023c69d104d840da9da51fa726c289357"+
+"7af6f51051f8d4c91a45ea11d7c04b632c4b81280796af677e96e6c55830418f13914d02edd54c"+
+"94dbd6f15fab2964dc14003264dcdeaded7fc878dbc9f9908f2ad0a5b8360fccd241f68aee0eb2"+
+"97eeb2a3")]
